200c33b4b6817e1f79ed0c01e992414ebd3d52bb
jaehyun <wogustkfkd11@gmail.com>
2022-12-31 01:02:23 +0900

    싱글톤

---
+++ src/main/java/hello/servlet/domain/member/MemberRepository.java
@@ -0,0 +1,41 @@
+package hello.servlet.domain.member;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 동시성 문제가 고려되어 있지 않음, 실무에서는 ConcurrentHashMap, AtomicLong 사용 고려
+ */
+public class MemberRepository {
+    //static으로 생성해서 new가 많아도 map과 sequence는 단 1개만 존재함. 싱글톤으로 정의했기 때문에 굳이 없어도 되긴 함.
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+    private static final MemberRepository instance = new MemberRepository();
+
+    public static MemberRepository getInstance() {
+        return instance;
+    }
+
+    private MemberRepository() {
+    }
+
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    public Member findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
