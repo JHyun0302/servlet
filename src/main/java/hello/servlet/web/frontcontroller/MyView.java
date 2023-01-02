b2bf5ffd8faa8ae0a739b04a6ef98e48427bb543
jaehyun <wogustkfkd11@gmail.com>
2023-01-02 23:06:43 +0900

    view 역할

---
+++ src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -0,0 +1,32 @@
+package hello.servlet.web.frontcontroller;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+public class MyView {
+    private String viewPath;
+
+    public MyView(String viewPath) {
+        this.viewPath = viewPath;
+    }
+
+    public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        modelToRequestAttribute(model, request);
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath); //Controller에서 view로 이동
+        dispatcher.forward(request, response); //다른 서블릿이나 JSP로 이동하는 기능: request에 담긴 값 get으로 사용
+    }
+
+    private static void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest request) {
+        model.forEach((key, value) -> request.setAttribute(key, value));
+        //model에 있는 모든 data 꺼내서 request에 (key, value)로 저장
+    }
+}
