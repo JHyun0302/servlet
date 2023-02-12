959ee3870321bd3b3d0c1a807c40efb6661c7973
jaehyun <wogustkfkd11@gmail.com>
2023-02-12 22:11:21 +0900

    설명 추가

--- src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
+++ src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -21,7 +21,7 @@ public class ResponseHeaderServlet extends HttpServlet {
 //        response.setHeader("Content-Type", "text/plain; charset=utf-8"); // content랑 똑같음
         response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); //캐시 무효화
         response.setHeader("Pragma", "no-cache"); //캐시 무효화
-        response.setHeader("my-header", "hello"); //내가 만든 임의의 헤더
+        response.setHeader("my-header", "Hello"); //내가 원하는 임의의 헤더 만들기
 
         //[Header 편의 메서드]
         content(response);
@@ -30,10 +30,13 @@ public class ResponseHeaderServlet extends HttpServlet {
 
         //[message body]
         PrintWriter writer = response.getWriter();
-        writer.println("23년에도 화이팅 해주세요");
+        writer.println("올~ 석사는 영어도 잘하네?");
     }
 
     private void content(HttpServletResponse response) {
+        /**
+         * contentType 편의 메서드
+         */
         //Content-Type: text/plain;charset=utf-8
         //Content-Length: 2
         //response.setHeader("Content-Type", "text/plain;charset=utf-8");
@@ -43,6 +46,9 @@ public class ResponseHeaderServlet extends HttpServlet {
     }
 
     private void cookie(HttpServletResponse response) {
+        /**
+         * Cookie 편의 메서드
+         */
         //Set-Cookie: myCookie=good; Max-Age=600;
         // response.setHeader("Set-Cookie", "myCookie=good; Max-Age=600");
         Cookie cookie = new Cookie("myCookie", "good");
@@ -51,11 +57,14 @@ public class ResponseHeaderServlet extends HttpServlet {
     }
 
     private void redirect(HttpServletResponse response) throws IOException {
+        /**
+         * Redirect 편의 메서드
+         */
         //Status Code 302   -> Location으로 사이트 이동해서 조회
         //Location: /basic/hello-form.html
 
-        //response.setStatus(HttpServletResponse.SC_FOUND); //302
-        //response.setHeader("Location", "/basic/hello-form.html");
+//        response.setStatus(HttpServletResponse.SC_FOUND); //302
+//        response.setHeader("Location", "/basic/hello-form.html");
         response.sendRedirect("/basic/hello-form.html");
     }
 }
