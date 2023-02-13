e54097d853498844ba8b4cbbf0e1766ab49abef2
jaehyun <wogustkfkd11@gmail.com>
2023-02-13 12:56:50 +0900

    ControllerV1 Interface를 이용한 FrontController 도입!

--- src/main/java/hello/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
+++ src/main/java/hello/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
@@ -13,9 +13,13 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * ControllerV1 Interface를 이용한 FrontController 도입!
+ * "/front-controller/v1"를 포함한 하위 모든 요청은 이 서블릿에서 받아들인다
+ */
 @WebServlet(name = "frontControllerServletV1", urlPatterns = "/front-controller/v1/*")
 public class FrontControllerServletV1 extends HttpServlet {
-    private Map<String, ControllerV1> controllerMap = new HashMap<>();
+    private Map<String, ControllerV1> controllerMap = new HashMap<>(); //<매핑 URL, 호출될 컨트롤러>
 
     public FrontControllerServletV1() {
         controllerMap.put("/front-controller/v1/members/new-form", new MemberFormControllerV1()); // new-form.jsp 실행되면 new MemberFormControllerV1()이 실행됨
@@ -29,7 +33,7 @@ public class FrontControllerServletV1 extends HttpServlet {
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("FrontControllerServletV1.service");
 
-        String requestURI = request.getRequestURI();///front-controller/V1/members/new-form 경로 얻을 수 있음
+        String requestURI = request.getRequestURI();// "/front-controller/V1/members/new-form" 경로 얻을 수 있음
 
         ControllerV1 controller = controllerMap.get(requestURI); //new MemberFormControllerV1() 객체 인스턴스가 반환됨
         if (controller == null) {
