b4179577b275f2c5957f208e0d2dde1d082ebf30
jaehyun <wogustkfkd11@gmail.com>
2023-02-15 10:22:24 +0900

    코드 변경

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -7,17 +7,8 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @ServletComponentScan //현재 패키지부터 하위패키지까지 쭉 서블릿으로 등록시켜줌
 @SpringBootApplication
 public class ServletApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ServletApplication.class, args);
     }
-
-    /**
-     * application.properties에서 설정해놓으면 자동으로 springboot가 해줌
-     */
-/*    @Bean
-    ViewResolver InternalResourceViewResolver() {
-        return new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
-    }*/
 
 }
